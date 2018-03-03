fe351048ae61b89618442a752afc2b6ee8e658e5
jiro.kirameki <edcated2111@aurora.ocn.ne.jp>
2018-03-03 05:12:24 +0000

    rakuten api

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,35 @@
+class ItemsController < ApplicationController
+  def new
+    @items=[]
+    
+    @keyword=params[:keyword]
+    if @keyword.present? #
+      results=RakutenWebService::IchibaItems.search({
+        keyword: @keyword,
+        imageFlag: 1,
+        hits :20,
+      })
+      
+      results.each do |result|
+        item=Item.find_or_initialize_by(read(result))
+        @items << item
+      end
+    end
+  end
+  
+  private
+  
+  def read(result)
+    code=result['itemCode']
+    name=result['itemName']
+    url=result['itemUrl']
+    image_url['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+    
+    return {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end
+end
